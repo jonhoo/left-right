b03c421e9ca639dd4bd05eeaa76f4750098c8fbe
Jon Gjengset <jon@thesquareplanet.com>
2017-10-18 21:45:06 -0400

    Significantly simplify epoch checking code

--- src/write.rs
+++ src/write.rs
@@ -46,7 +46,6 @@ where
     swap_index: usize,
     r_handle: ReadHandle<K, V, M, S>,
     last_epochs: Vec<usize>,
-    epochs_checked: Vec<bool>,
     meta: M,
     first: bool,
     second: bool,
@@ -68,7 +67,6 @@ where
         swap_index: 0,
         r_handle: r_handle,
         last_epochs: Vec::new(),
-        epochs_checked: Vec::new(),
         meta: m,
         first: true,
         second: false,
@@ -100,107 +98,76 @@ where
         // unless they have finished reading.
         let epochs = self.w_handle.as_ref().unwrap().epochs.clone();
         let epochs = epochs.lock().unwrap();
-        let mut first = false;
+        self.last_epochs.resize(epochs.len(), 0);
+
+        let mut iter = 0;
+        let mut starti = 0;
         let high_bit = 1usize << (mem::size_of::<usize>() * 8 - 1);
-        loop {
-            let last_epochs = &self.last_epochs;
+        'retry: loop {
+            // read all and see if all have changed (which is likely)
+            for i in starti..epochs.len() {
+                let now = epochs[i].load(atomic::Ordering::Acquire);
+                if (now != self.last_epochs[i]) | (now & high_bit != 0) | (now == 0) {
+                    // reader must have seen last swap
+                } else {
+                    // reader may not have seen swap
+                    // continue from this reader's epoch
+                    starti = i;
 
-            let mut all_left = false;
-            if first {
-                // fast path -- read all and see if all have changed (which is likely)
-                let epochs_checked = &mut self.epochs_checked;
-                all_left = last_epochs.iter().enumerate().all(|(i, &e)| {
-                    let now = epochs[i].load(atomic::Ordering::Acquire);
-                    epochs_checked[i] = now == 0 || now != e || now & high_bit != 0;
-                    epochs_checked[i]
-                });
-                first = false;
-            }
-
-            if !all_left {
-                // slow path -- only some have changed
-                all_left = self.epochs_checked
-                    .iter_mut()
-                    .enumerate()
-                    .all(|(i, epoch)| {
-                        if *epoch {
-                            return true;
-                        }
-
-                        let last = last_epochs[i];
-                        let now = epochs[i].load(atomic::Ordering::Acquire);
-                        if last != now {
-                            // epoch increased or high bit now set, both indicate reader has left.
-                            // may have entered again, but if so it must have read new pointer.
-                            *epoch = true;
-                            return true;
-                        }
-
-                        if now == 0 {
-                            // reader has never done a read
-                            *epoch = true;
-                            return true;
-                        }
-
-                        if now & high_bit != 0 {
-                            // high bit set, and previous value was the same, so reader has indeed
-                            // left.
-                            *epoch = true;
-                            return true;
-                        }
-
-                        false
-                    });
-            }
-
-            if all_left {
-                // all the readers have left!
-                // we can safely bring the w_handle up to date.
-                let w_handle = self.w_handle.as_mut().unwrap();
-
-                if self.second {
-                    use std::mem;
-                    // before the first refresh, all writes went directly to w_handle. then, at the
-                    // first refresh, r_handle and w_handle were swapped. thus, the w_handle we
-                    // have now is empty, *and* none of the writes in r_handle are in the oplog.
-                    // we therefore have to first clone the entire state of the current r_handle
-                    // and make that w_handle, and *then* replay the oplog (which holds writes
-                    // following the first refresh).
-                    //
-                    // this may seem unnecessarily complex, but it has the major advantage that it
-                    // is relatively efficient to do lots of writes to the evmap at startup to
-                    // populate it, and then refresh().
-                    let r_handle = unsafe {
-                        Box::from_raw(self.r_handle.inner.load(atomic::Ordering::Relaxed))
-                    };
-                    w_handle.data = r_handle.data.clone();
-                    mem::forget(r_handle);
-                }
-
-                // the w_handle map has not seen any of the writes in the oplog
-                // the r_handle map has not seen any of the writes following swap_index
-                if self.swap_index != 0 {
-                    // we can drain out the operations that only the w_handle map needs
-                    // NOTE: the if above is because drain(0..0) would remove 0
-                    for op in self.oplog.drain(0..self.swap_index) {
-                        Self::apply_op(w_handle, op);
+                    // how eagerly should we retry?
+                    if iter != 20 {
+                        iter += 1;
+                    } else {
+                        thread::yield_now();
                     }
+
+                    continue 'retry;
                 }
-                // the rest have to be cloned because they'll also be needed by the r_handle map
-                for op in self.oplog.iter().cloned() {
+            }
+
+            // all the readers have left!
+            // we can safely bring the w_handle up to date.
+            let w_handle = self.w_handle.as_mut().unwrap();
+
+            if self.second {
+                use std::mem;
+                // before the first refresh, all writes went directly to w_handle. then, at the
+                // first refresh, r_handle and w_handle were swapped. thus, the w_handle we
+                // have now is empty, *and* none of the writes in r_handle are in the oplog.
+                // we therefore have to first clone the entire state of the current r_handle
+                // and make that w_handle, and *then* replay the oplog (which holds writes
+                // following the first refresh).
+                //
+                // this may seem unnecessarily complex, but it has the major advantage that it
+                // is relatively efficient to do lots of writes to the evmap at startup to
+                // populate it, and then refresh().
+                let r_handle =
+                    unsafe { Box::from_raw(self.r_handle.inner.load(atomic::Ordering::Relaxed)) };
+                w_handle.data = r_handle.data.clone();
+                mem::forget(r_handle);
+            }
+
+            // the w_handle map has not seen any of the writes in the oplog
+            // the r_handle map has not seen any of the writes following swap_index
+            if self.swap_index != 0 {
+                // we can drain out the operations that only the w_handle map needs
+                // NOTE: the if above is because drain(0..0) would remove 0
+                for op in self.oplog.drain(0..self.swap_index) {
                     Self::apply_op(w_handle, op);
                 }
-                // the w_handle map is about to become the r_handle, and can ignore the oplog
-                self.swap_index = self.oplog.len();
-                // ensure meta-information is up to date
-                w_handle.meta = self.meta.clone();
-                w_handle.mark_ready();
-
-                // w_handle (the old r_handle) is now fully up to date!
-                break;
-            } else {
-                thread::yield_now();
             }
+            // the rest have to be cloned because they'll also be needed by the r_handle map
+            for op in self.oplog.iter().cloned() {
+                Self::apply_op(w_handle, op);
+            }
+            // the w_handle map is about to become the r_handle, and can ignore the oplog
+            self.swap_index = self.oplog.len();
+            // ensure meta-information is up to date
+            w_handle.meta = self.meta.clone();
+            w_handle.mark_ready();
+
+            // w_handle (the old r_handle) is now fully up to date!
+            break;
         }
 
         // at this point, we have exclusive access to w_handle, and it is up-to-date with all
@@ -221,11 +188,8 @@ where
         let r_handle = self.r_handle.inner.swap(w_handle, atomic::Ordering::SeqCst);
         let r_handle = unsafe { Box::from_raw(r_handle) };
 
-        self.last_epochs.clear();
-        self.epochs_checked.clear();
-        for e in epochs.iter() {
-            self.last_epochs.push(e.load(atomic::Ordering::Acquire));
-            self.epochs_checked.push(false);
+        for i in 0..epochs.len() {
+            self.last_epochs[i] = epochs[i].load(atomic::Ordering::Acquire);
         }
 
         // NOTE: at this point, there are likely still readers using the w_handle we got
