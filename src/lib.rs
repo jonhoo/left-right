6dc68a96477ada00c86602e010f54c21a5740701
Taras Tsugrii <taras.tsugriy@gmail.com>
2023-08-21 11:38:33 -0700

    [nit] Fix a comment typo.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! A concurrency primitive for high concurrency reads over a single-writer data structure.
 //!
 //! The primitive keeps two copies of the backing data structure, one that is accessed by readers,
-//! and one that is access by the (single) writer. This enables all reads to proceed in parallel
+//! and one that is accessed by the (single) writer. This enables all reads to proceed in parallel
 //! with minimal coordination, and shifts the coordination overhead to the writer. In the absence
 //! of writes, reads scale linearly with the number of cores.
 //!
