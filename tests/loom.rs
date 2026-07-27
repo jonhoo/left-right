911ed698d15f279f8fb7777250c6611e973b1a53
liang <757410129@qq.com>
2026-07-27 15:10:40 +0800

    release epoch registry while waiting

--- tests/loom.rs
+++ tests/loom.rs
@@ -26,4 +26,28 @@ mod loom_tests {
             assert_eq!(1, val);
         });
     }
+
+    #[test]
+    fn create_handle_while_writer_waits() {
+        loom::model(|| {
+            let (mut w, r) = left_right::new::<i32, CounterAddOp>();
+            w.publish();
+
+            let factory = r.factory();
+            let guard = r.enter().unwrap();
+            w.publish();
+
+            let writer = thread::spawn(move || {
+                w.publish();
+            });
+            thread::yield_now();
+
+            let creator = thread::spawn(move || factory.handle());
+            let handle = creator.join().unwrap();
+            drop(handle);
+
+            drop(guard);
+            writer.join().unwrap();
+        });
+    }
 }
